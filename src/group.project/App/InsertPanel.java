812bd339f0d2e400aafd8d7dd2971eee414e3cce
edric101 <edric101@students.cs.ubc.ca>
2024-03-29 12:22:29 -0700

    Insert Player works on GUI, added login window

--- src/group.project/App/InsertPanel.java
+++ src/group.project/App/InsertPanel.java
@@ -1,23 +1,37 @@
+package group.project.App;
+
+import group.project.delegates.TransactionDelegate;
+import group.project.model.Player2Model;
+import group.project.model.Player4Model;
+import group.project.model.Player6Model;
+import group.project.model.Player7Model;
+
 import javax.swing.*;
 import java.awt.*;
+import java.util.ArrayList;
+import java.util.Objects;
 
 public class InsertPanel {
-    public JPanel getInsertPanel() {
+    private ArrayList<JTextField> fields;
+    private TransactionDelegate delegate = null;
+    public JPanel getInsertPanel(TransactionDelegate delegate) {
+        this.delegate = delegate;
         JPanel panel = new JPanel();
+        fields = new ArrayList<>();
         panel.setLayout(new GridLayout(0, 2));
 
         // Add labels and text fields to the panel
 
         // We can re-use this
         String[][] setup = {
-                {"Player Name", ""},
-                {"Server ID", "1"},
+                {"Player Name *", ""},
+                {"Server ID *", "1"},
                 {"Weapon ID", ""},
                 {"Weapon Name", ""},
-                {"Level", "1"},
-                {"EXP", "0"},
-                {"Health", "50"},
-                {"Mana", "40"},
+                {"Level *", "1"},
+                {"EXP *", "0"},
+                {"Health *", "50"},
+                {"Mana *", "40"},
                 {"Guild Name", ""},
                 {"Role", ""}
         };
@@ -28,14 +42,50 @@ public class InsertPanel {
             textField1.setText(setup[i][1]);
             panel.add(label1);
             panel.add(textField1);
+            fields.add(textField1);
         }
 
         // Add button to the panel for producing the alert
         JButton alertButton = new JButton("Add");
-        alertButton.addActionListener(e -> showAlert(panel));
+        alertButton.addActionListener(e -> doQuery(panel));
         panel.add(alertButton);
 
         return panel;
+    }
+
+    private void doQuery(JPanel panel) {
+        // check for invalid values
+
+        if(Objects.equals(fields.get(0).getText(), "") || Objects.equals(fields.get(1).getText(), "")
+        || Objects.equals(fields.get(4).getText(), "") || Objects.equals(fields.get(5).getText(), "")
+                || Objects.equals(fields.get(6).getText(), "") || Objects.equals(fields.get(7).getText(), "")) {
+            // throw some error here?
+            JOptionPane.showMessageDialog(panel, "Error: please fill in all required fields.");
+            return;
+        }
+
+        String pname = fields.get(0).getText();
+        int sid = Integer.parseInt(fields.get(1).getText());
+        String wname = fields.get(3).getText();
+
+        int wid;
+        if(Objects.equals(fields.get(2).getText(), "")) {
+            wid = -1;
+        } else {
+            wid = Integer.parseInt(fields.get(2).getText());
+        }
+        int exp = Integer.parseInt(fields.get(5).getText());
+        String gname = fields.get(8).getText();
+        String role = fields.get(9).getText();
+        int lvl = Integer.parseInt(fields.get(4).getText());
+        int mana = Integer.parseInt(fields.get(7).getText());
+        int health = Integer.parseInt(fields.get(6).getText());
+
+        Player7Model p7 = new Player7Model(pname, sid, wname, wid, exp, gname, role);
+        Player6Model p6 = new Player6Model(exp, health);
+        Player4Model p4 = new Player4Model(exp, mana);
+        Player2Model p2 = new Player2Model(exp, lvl);
+        delegate.insertPlayer(p2, p4, p6, p7);
     }
 
     private void showAlert(JPanel panel) {
--- src/group.project/App/MyApplication.java
+++ src/group.project/App/MyApplication.java
@@ -1,7 +1,21 @@
+package group.project.App;
+
+import group.project.database.DatabaseConnectionHandler;
+import group.project.delegates.LoginDelegate;
+import group.project.delegates.TransactionDelegate;
+import group.project.model.Player2Model;
+import group.project.model.Player4Model;
+import group.project.model.Player6Model;
+import group.project.model.Player7Model;
+
 import javax.swing.*;
 import java.awt.*;
 
-public class MyApplication {
+public class MyApplication implements TransactionDelegate, LoginDelegate {
+
+    private DatabaseConnectionHandler dbHandler = null;
+    private LoginWindow loginWindow = null;
+
     private JFrame frame;
     private JPanel mainPanel;
 
@@ -10,33 +24,9 @@ public class MyApplication {
     private JPanel insertPanel;
 
     public MyApplication() {
-        // Initialize the frame
-        frame = new JFrame("My Application");
-        frame.setSize(400, 300);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-        // Create main panel
-        mainPanel = new JPanel();
-        mainPanel.setLayout(new FlowLayout());
-
-        // DEMO UPDATE - REMAKE THIS
-//        JButton button = new JButton("+ Add Item");
-//        UpdatePanel up = new UpdatePanel();
-//        updatePanel = up.getUpdatePanel();
-//        button.addActionListener(e -> switchScreen(updatePanel));
-
-        JButton insertButton = new JButton("Insert Player");
-        InsertPanel ip = new InsertPanel();
-        insertPanel = ip.getInsertPanel();
-        insertButton.addActionListener(e -> switchScreen(insertPanel));
-
-        // Add all Buttons
-//        mainPanel.add(button); // replace this
-        mainPanel.add(insertButton);
-
-        // Finish
-        frame.add(mainPanel);
-        frame.setVisible(true);
+        dbHandler = new DatabaseConnectionHandler();
+        loginWindow = new LoginWindow();
+        loginWindow.showFrame(this);
     }
 
     private void switchScreen(JPanel panel) {
@@ -49,6 +39,59 @@ public class MyApplication {
         // Repaint frame
         frame.revalidate();
         frame.repaint();
+    }
+
+    public void insertPlayer(Player2Model p2, Player4Model p4, Player6Model p6, Player7Model p7) {
+        dbHandler.insertPlayer(p2, p4, p6, p7);
+    }
+
+    public void deleteNPC(int nidToDelete, String nameToDelete) {
+        dbHandler.deleteNPC(nidToDelete, nameToDelete);
+    }
+
+    public Integer[] getRanksWithMostGuilds() {
+        return dbHandler.getRanksWithMostGuilds();
+    }
+
+    public void login(String username, String password) {
+        boolean connected = dbHandler.login(username, password);
+        if(connected) {
+            loginWindow.dispose();
+            frame = new JFrame("My Application");
+            frame.setSize(400, 300);
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+            // Create main panel
+            mainPanel = new JPanel();
+            mainPanel.setLayout(new FlowLayout());
+
+
+            // DEMO UPDATE - REMAKE THIS
+//        JButton button = new JButton("+ Add Item");
+//        UpdatePanel up = new UpdatePanel();
+//        updatePanel = up.getUpdatePanel();
+//        button.addActionListener(e -> switchScreen(updatePanel));
+
+            JButton insertButton = new JButton("Insert Player");
+            InsertPanel ip = new InsertPanel();
+            insertPanel = ip.getInsertPanel(this);
+            insertButton.addActionListener(e -> switchScreen(insertPanel));
+
+            // Add all Buttons
+//        mainPanel.add(button); // replace this
+            mainPanel.add(insertButton);
+
+            // Finish
+            frame.add(mainPanel);
+            frame.setVisible(true);
+        } else {
+            loginWindow.handleLoginFailed();
+            if (loginWindow.hasReachedMaxLoginAttempts()) {
+                loginWindow.dispose();
+                System.out.println("Max login attempts reached.");
+                System.exit(-1);
+            }
+        }
     }
 
     public static void main(String[] args) {
